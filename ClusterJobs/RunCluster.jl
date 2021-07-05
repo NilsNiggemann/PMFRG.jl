cfdc1dd2d13216e611973bc3f272598e6912a39c
niggeni <nils.niggemann@physik.fu-berlin.de>
2021-07-05 13:07:10 +0200

    outsourced ClusterJobs

--- ClusterJobs/RunCluster.jl
+++
@@ -1,9 +0,0 @@
-include("ClusterUtils.jl")
-# include("../src/PMFRG.jl")
-@time @everywhere using PMFRG
-using SpinFRGLattices
-# include("../TwoLoopPMFRG.jl")
-# using .SimpleCubic
-##
-System = getPolymer(4)
-DistributedTSweep(0.5:0.1:2,System,N=24,Ngamma = 300,add_name = "2",accuracy = 1E-9)

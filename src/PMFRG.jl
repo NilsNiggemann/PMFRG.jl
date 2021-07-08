d1c672dc5886de7a25eae99715a6752b2715247b
niggeni <nils.niggemann@physik.fu-berlin.de>
2021-07-08 09:39:17 +0200

    package update and version num

--- src/PMFRG.jl
+++ src/PMFRG.jl
@@ -2,7 +2,7 @@ module PMFRG
     using SpinFRGLattices,OrdinaryDiffEq,DiffEqCallbacks,Parameters,Printf,RecursiveArrayTools,LoopVectorization
     export SolveFRG,Params,double,BS3,Vern7,DP5,version
 
-    version() = """v.1.0.2"""
+    version() = """v.1.0.3"""
     # Essentials
     include("Globals.jl")
     include("VertexFunctions_Dense.jl")

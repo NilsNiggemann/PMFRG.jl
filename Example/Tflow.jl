28d5d8d901dfd31015ec1eb66203fb919928da65
Nils <nils.niggemann@fu-berlin.de>
2023-10-14 12:47:39 +0200

    change par

--- Example/Tflow.jl
+++ Example/Tflow.jl
@@ -1,3 +1,3 @@
 using SpinFRGLattices, PMFRG
-Par = Params(getPolymer(2),N=10,T_min = 0.2,T_max =exp(7),accuracy = 1e-5)
+Par = Params(getPolymer(2),N=10,T_min = 0.4,T_max =exp(7),accuracy = 1e-5)
 SolveFRG(Par)

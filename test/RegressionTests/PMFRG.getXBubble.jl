5257ea62d58d850372051f8cdee25d2ca73385e2
Michele Mesiti <michele.mesiti@kit.edu>
2023-12-08 13:21:57 +0100

    getXBubble! regr tests: laxed condition, Refactor

--- test/RegressionTests/PMFRG.getXBubble.jl
+++ test/RegressionTests/PMFRG.getXBubble.jl
@@ -4,58 +4,5 @@ using Serialization
 thisdir = dirname(@__FILE__)
 data = deserialize(joinpath(thisdir,"PMFRG.getXBubble.data"))
 
+include("PMFRG.getXBubble.common.jl")
 
-#"Function that flattens 'args' into a Vector{Any}, so that "
-function extractX(args)
-    Workspace, Lam = args
-    # We do not care about Par because it is immutable and constant
-    # and about Buffer because we do not care about the contents
-    (;State,Deriv,X) = Workspace
-
-    #(;a,b,c,Ta,Tb,Tc,Td) = X
-
-    return X
-end
-
-
-function compare_arguments_post(args_post_exp, args_post)
-    Xexp = args_post_exp[1].X
-    X    = args_post[1].    X
-
-    result = true
-    for field in fieldnames(typeof(X))
-        val = getfield(X,field)
-        expval = getfield(Xexp,field)
-        # This might be too strict
-        @assert typeof(expval) == Array{Float64,4}
-        if val != expval
-            absdiff = abs.(val.-expval)
-            print("Test failed: $field differs: ")
-            print("Absolute Difference: ", sum(absdiff), ", ")
-            print("Max Difference: ", maximum(absdiff), "\n")
-            different_val_places = val .!= expval
-            println("First computed values: ")
-            println(first(val[different_val_places],5))
-            println("First computed values - reinterpreted: ")
-            println(reinterpret(UInt64,first(val[different_val_places],5)))
-            println("First expected values: ")
-            println(first(expval[different_val_places],5))
-            println("First expected values - reinterpreted: ")
-            println(reinterpret(UInt64,first(expval[different_val_places],5)))
-            result = false
-        end
-    end
-    result
-end
-
-function test_getXBubble()
-    @testset verbose = true "Tests for getXBubble!" begin
-            @testset for i = 1:length(data["return_value"])
-                    return_value = (data["return_value"])[i]
-                    arguments = (data["arguments"])[i]
-                    arguments_post = (data["arguments_post"])[i]
-                    PMFRG.getXBubble!(arguments...)
-                    @test compare_arguments_post(arguments, arguments_post)
-                    end
-        end
-end
--- test/RegressionTests/PMFRG.getXBubbleMPI.jl
+++ test/RegressionTests/PMFRG.getXBubbleMPI.jl
@@ -11,48 +11,10 @@ using MPI
 thisdir = dirname(@__FILE__)
 data = deserialize(joinpath(thisdir,"PMFRG.getXBubble.data"))
 
+include("PMFRG.getXBubble.common.jl")
+
 MPI.Init()
 
-#"Function that flattens 'args' into a Vector{Any}, so that "
-function extractX(args)
-    Workspace, Lam = args
-    # We do not care about Par because it is immutable and constant
-    # and about Buffer because we do not care about the contents
-    (;State,Deriv,X) = Workspace
-
-    #(;a,b,c,Ta,Tb,Tc,Td) = X
-
-    return X
-end
-
-
-function compare_arguments_post(args_post_exp, args_post)
-    Xexp = args_post_exp[1].X
-    X    = args_post[1].    X
-
-    result = true
-    for field in fieldnames(typeof(X))
-        val = getfield(X,field)
-        expval = getfield(Xexp,field)
-        if val != expval
-            print("Test failed: $field differs :")
-            print("Absolute Difference: ", sum( abs.(val.-expval)), "\n")
-            print("Number of wrong values: ", sum( (val .!= expval)), "\n")
-            print("size of field:", size(val))
-            result = false
-        end
-    end
-    result
-end
-
-@testset verbose = true "Tests for getXBubble!" begin
-        @testset for i = 1:length(data["return_value"])
-                return_value = (data["return_value"])[i]
-                arguments = (data["arguments"])[i]
-                arguments_post = (data["arguments_post"])[i]
-                PMFRG.getXBubble!(arguments...)
-                @test compare_arguments_post(arguments, arguments_post)
-                end
-    end
+test_getXBubble()
 
 MPI.Finalize()

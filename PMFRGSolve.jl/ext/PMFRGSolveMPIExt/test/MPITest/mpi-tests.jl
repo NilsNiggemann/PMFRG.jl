b5f1ffd399acfa2d1d50b51ffdf95b3ae5b261d5
Michele Mesiti <michele.mesiti@kit.edu>
2024-11-17 18:14:12 +0100

    In mpi test, print output only of first rank.

--- PMFRGSolve.jl/ext/PMFRGSolveMPIExt/test/MPITest/mpi-tests.jl
+++ PMFRGSolve.jl/ext/PMFRGSolveMPIExt/test/MPITest/mpi-tests.jl
@@ -9,15 +9,50 @@ function test_mpi_solve()
                 println("="^linelength)
                 title = " $testname "
                 subtitle = " \"$(basename(script))\" "
-                println("="^5 * title * "="^(linelength - 5 - length(title)))
-                println("="^5 * subtitle * "="^(linelength - 5 - length(subtitle)))
+                function print_line(string)
+                    println("="^5 * string * "="^(linelength - 5 - length(string)))
+                end
+
+                print_line(title)
+                print_line(subtitle)
+                print_line(" Output of first rank only (out of $n)")
                 println("="^linelength)
+
+            end
+            function create_mpi_shell_wrapper()
+                # From: https://www.open-mpi.org/community/lists/users/2012/02/18362.php
+                mpi_shell_wrapper_text = """
+#!/bin/bash
+ARGS=\$@
+if [[ -v OMPI_COMM_WORLD_RANK ]] # OpenMPI Case
+then
+   MY_MPI_RANK=\$OMPI_COMM_WORLD_RANK
+elif [[ -v PMI_RANK ]]           # MPICH Case, probably
+then
+   MY_MPI_RANK=\$PMI_RANK
+fi
+if [[ \$MY_MPI_RANK == 0 ]]
+then
+  \$ARGS
+else
+  \$ARGS 1>/dev/null 2>/dev/null
+fi
+"""
+                path = "./shell-wrapper-text.sh"
+                open(path, "w") do io
+                    write(io, mpi_shell_wrapper_text)
+                end
+                run(`chmod +x $path`)
+                path
             end
 
+
             print_header()
+            wrapper_path = create_mpi_shell_wrapper()
             @testset verbose = true "$testname" begin
                 p = run(ignorestatus(`$(mpiexec())
                                       -n $n
+                                      $wrapper_path
                                       $(Base.julia_cmd())
                                       --project=$(Base.active_project())
                                       $script`))
